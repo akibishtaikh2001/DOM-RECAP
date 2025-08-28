aa1df650f88b82d57d56e92161b4e1d319ca660f
akibishtaikh2001 <akibishtiakh2220@gmail.com>
2025-08-28 13:44:28 +0600

    Get confused by isNaN

---
+++ validation.js
@@ -0,0 +1,16 @@
+const number = 23;
+// string, number, boolean
+if (typeof number === 'number') {
+    console.log('value is a number');
+}
+else {
+    console.log('value is not a number');
+
+}
+
+
+const numbers = [3, 12, 78, 98];
+const student = { name: 'Rongila Rafsan', job: 'Khawadawa kora' }
+// console.log(Array.isArray(numbers));
+console.log(Array.isArray(number));
+console.log(typeof student);
